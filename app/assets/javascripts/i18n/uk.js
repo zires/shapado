a36fb972492dd3de94cb31fc2af53d9063bd424a
Siebrand Mazeland <s.mazeland@xs4all.nl>
2012-12-08 18:48:59 +0000

    Localisation updates from http://translatewiki.net.

--- app/assets/javascripts/i18n/uk.js
+++ app/assets/javascripts/i18n/uk.js
@@ -2,10 +2,12 @@
  *  Exported from translatewiki.net
  *
  * Translators:
+ *  - Base
  *  - Тест
  */
 
 var I18n = {
-	on_leave_page: "Ви можете втратити ваші зміни"
+	on_leave_page: "Ви можете втратити ваші зміни",
+	loading: "Завантаження…"
 };
 
