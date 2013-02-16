50bdfaab495cb22ce5879ab36bde6333304b909f
Siebrand Mazeland <s.mazeland@xs4all.nl>
2013-02-16 05:35:46 +0000

    Localisation updates from http://translatewiki.net.

---
+++ app/assets/javascripts/i18n/ast.js
@@ -0,0 +1,12 @@
+/** Messages for Asturian (asturianu)
+ *  Exported from translatewiki.net
+ *
+ * Translators:
+ *  - Xuacu
+ */
+
+var I18n = {
+	on_leave_page: "Podría perder los sos cambeos",
+	loading: "Cargando..."
+};
+
---
+++ app/assets/javascripts/i18n/roa-tara.js
@@ -0,0 +1,12 @@
+/** Messages for tarandíne (tarandíne)
+ *  Exported from translatewiki.net
+ *
+ * Translators:
+ *  - Joetaras
+ */
+
+var I18n = {
+	on_leave_page: "Tu puè perdere le cangiaminde tune",
+	loading: "Stoche a careche…"
+};
+
