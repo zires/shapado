9516d5ba073177f5787804350ff890b380e4633a
Siebrand Mazeland <s.mazeland@xs4all.nl>
2013-02-24 17:10:22 +0000

    Localisation updates from http://translatewiki.net.

--- app/assets/javascripts/i18n/br.js
+++ app/assets/javascripts/i18n/br.js
@@ -2,10 +2,12 @@
  *  Exported from translatewiki.net
  *
  * Translators:
+ *  - Fohanno
  *  - Siebrand
  */
 
 var I18n = {
-	on_leave_page: "Marteze emaoc\'h o vont da goll kemmoù bet degaset d\'ar bajenn-mañ"
+	on_leave_page: "Marteze emaoc\'h o vont da goll kemmoù bet degaset d\'ar bajenn-mañ",
+	loading: "O kargañ..."
 };
 
