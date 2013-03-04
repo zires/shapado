76fb499fa1ffccc49ae1a25dfdc1319aab56842c
Siebrand Mazeland <s.mazeland@xs4all.nl>
2013-03-04 11:46:32 +0000

    Localisation updates from http://translatewiki.net.

---
+++ app/assets/javascripts/i18n/ps.js
@@ -0,0 +1,11 @@
+/** Messages for Pashto (پښتو)
+ *  Exported from translatewiki.net
+ *
+ * Translators:
+ *  - Ahmed-Najib-Biabani-Ibrahimkhel
+ */
+
+var I18n = {
+	loading: "برسېرېدنې کې دی..."
+};
+
