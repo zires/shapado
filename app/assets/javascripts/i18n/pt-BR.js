78480a33b5b211f92d97e2c29a4b3379716dbf83
Siebrand Mazeland <s.mazeland@xs4all.nl>
2013-05-09 13:50:45 +0000

    Localisation updates from http://translatewiki.net.

--- app/assets/javascripts/i18n/pt-BR.js
+++ app/assets/javascripts/i18n/pt-BR.js
@@ -3,9 +3,11 @@
  *
  * Translators:
  *  - Giro720
+ *  - Luckas
  */
 
 var I18n = {
-	on_leave_page: "Você poderá perder suas alterações"
+	on_leave_page: "Você poderá perder suas alterações",
+	loading: "Carregando..."
 };
 
--- app/assets/javascripts/i18n/pt-PT.js
+++ app/assets/javascripts/i18n/pt-PT.js
@@ -3,9 +3,11 @@
  *
  * Translators:
  *  - Hamilton Abreu
+ *  - Luckas
  */
 
 var I18n = {
-	on_leave_page: "Poderá perder as suas alterações"
+	on_leave_page: "Poderá perder as suas alterações",
+	loading: "Carregando..."
 };
 
