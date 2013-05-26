6f4c29c70bd3d42d448d3f6b95f21b2e67675f83
Siebrand Mazeland <s.mazeland@xs4all.nl>
2013-05-26 09:55:08 +0000

    Localisation updates from http://translatewiki.net.

---
+++ app/assets/javascripts/i18n/vi.js
@@ -0,0 +1,12 @@
+/** Messages for Vietnamese (Tiếng Việt)
+ *  Exported from translatewiki.net
+ *
+ * Translators:
+ *  - Tuankiet65
+ */
+
+var I18n = {
+	on_leave_page: "Bạn có thể mất các thay đổi",
+	loading: "Đang tải…"
+};
+
